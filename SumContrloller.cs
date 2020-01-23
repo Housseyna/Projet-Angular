c26291441d637f85563410dc5aaf8b7d132204f9
Housseyna <60049777+Housseyna@users.noreply.github.com>
2020-01-23 17:17:19 -0500

    Add files via upload

---
+++ SumContrloller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace appNbAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+
+    public class SumController : ControllerBase
+        {
+                // Get api/values
+                [HttpGet]
+                public ActionResult<int>Get([FromQuery] int a, [FromQuery] int b)
+                    {
+                        
+                        return (a + b);
+                    }
+        }
+
+        //public ActionResult<IEnumerable<string>> Get()
+       // {
+        //    return new string[] { "value1", "value2" };
+       // }
+
+        // Other code omitted...
+  
+}
